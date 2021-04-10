9e22b6f9ffa3ee4dedd9afa9c4d46f32babbaa8b
anass99t <anass.taher@gmail.com>
2021-04-10 13:54:39 +0100

    Single File Components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <nav>
+      <HomeLink />
+      <a href="/about">About</a>
+      <a href="/content">content</a>
+    </nav>
+    <p>Welcome to our <HomeLink /> page! </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HomeLink from './components/HomeLink.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    HomeLink
   }
 }
 </script>
---
+++ src/components/HomeLink.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'HomeLink'
+}
+</script>
+
+<template>
+    <div>
+        <a href="/">Home</a>
+    </div>
+</template>
+
+<style>
+    a {
+        text-decoration: none;
+    }
+</style>
