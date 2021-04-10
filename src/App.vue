4b8c88e2753cb02f0b4eebabda29f89c91396fe9
anass99t <anass.taher@gmail.com>
2021-04-10 15:10:11 +0100

    route & dynamic route

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-
-    <nav>
-      <HomeLink />
-      <NavLink url='/about' text='About'/>
-      <NavLink url='/contact' text='Contact'/>
-    </nav>
-    <p>Welcome to our <HomeLink /> page! </p>
-
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import HomeLink from './components/HomeLink.vue'
-import NavLink from './components/NavLink.vue'
-
-export default {
-  name: 'App',
-  components: {
-    // HelloWorld,
-    HomeLink,
-    NavLink,
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
@@ -35,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/ItemDetail.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Item selected : {{ $route.params.name }}</h1>
+</template>
